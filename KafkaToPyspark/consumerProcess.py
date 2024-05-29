4a69a7fa680598f2a3ba1bf96adf75d656368382
Med Walid <kuro.walid.17@gmail.com>
2024-05-29 14:45:00 +0100

    go

--- KafkaToPyspark/consumerProcess.py
+++ KafkaToPyspark/consumerProcess.py
@@ -4,23 +4,53 @@ from json import loads
 from pymongo import MongoClient
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from pyspark.ml import ALSModel
 
 
-# Establish connection to MongoDB
-client = MongoClient('localhost', 27017)
+"""
+Create a MongoClient object that represents the connection to the MongoDB server, 
+localhost specifies that the server is running on the local machine and the 2701 
+parameteris the defaukt port number for MongoDB to listen for connections 
+(The port to be used to access this service)
+"""
+client = MongoClient('localhost', 27017) 
+
+"""
+ This line selects a specific database, bigdata_project, from the MongoDB server
+If it doesn't exist, it will be created when you insert data.
+""" 
 db = client['bigdata_project'] 
-collection = db['tweets'] 
+
+"""
+This line selects a collection from the database bigdata_project. 
+If it doesn't exist, if will be created when you insert data.
+"""
+collection = db['User-item ratings'] 
 
 
 
-# Assuming you have a SparkSession already created
+# Assuming you have a SparkSession already created - Building the Spark session
 spark = SparkSession.builder \
-    .appName("classify tweets") \
-    .getOrCreate()
+	.appName("Predict ratings") \
+	.getOrCreate()
+
+
+"""
+Sparksession is a class that represents an entry point API for Spark functionality.
+builder is a method of the sparksession class is used to return a builder object that 
+can be used to configure various spark properties
+appname() is a method of the builder object that is used to specify the application
+name, it is used to identify the application in the spark cluster
+getOrCreate() is a method of the builder object that retrieves an existing sparksession
+and if it doesn't exist it creates one, ensuring one spark session is created per 
+application
+--> the sparksession object is an entry point to interacting with spark and
+performing various operatiions on distributed data
+"""
 
 
 # Load the model
-loadedALSModel = ALSModel.load("path/to/als_model")
+loadedALSModel = ALSModel.load("als_model")
 
 
 def inputPreprocessing(text):
@@ -29,13 +59,19 @@ def inputPreprocessing(text):
 
 
 # Kafka Consumer
-consumer = KafkaConsumer(
-    'numtest',
-    bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='my-group',
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+consumer = KafkaConsumer( # A class that allows you to consume messages from a Kafka cluster
+    'numtest', # Specify the topic to consume from
+    bootstrap_servers=['localhost:9092'], # Specify the list of kafka broker addresses that the consumer should connect to
+    auto_offset_reset='earliest', # The consumer will start consuming topics from the earlires possible offset, this is specified 
+								  # to determine what happens when the consumer starts consuming messages for the first time or 
+                                  # when there is no valid offset 
+    enable_auto_commit=True,      # Whether the consumer should commit offsets automaticall
+    group_id='my-group',		# An identifier for which group the consumer belongs to, groups help multiple consumers work 
+    							# together to consume messages from a topic
+    value_deserializer=lambda x: loads(x.decode('utf-8'))) # is everything sent should be an array of bytes? 
+								# This is a function that deserializes the message value, the default is to return the message as a byte array
+                                # the loads function is used to desrialize the message, in this case it converts a JSON string into
+                                # a python object	
 
 
 for message in consumer:
